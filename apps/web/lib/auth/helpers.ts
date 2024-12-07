52f9716cbc1aed8d1d8736cc14a5c6eaf7c9c716
Jean <jean.carlosmolossi@gmail.com>
2024-12-06 22:40:43 -0300

    feat: add get logger user helper

--- apps/web/lib/auth/helpers.ts
+++ apps/web/lib/auth/helpers.ts
@@ -1,6 +1,9 @@
 'use server'
 
 import { createClient } from '@/lib/supabase/server'
+import { HOME_DOMAIN } from '@local/utils'
+import { redirect } from 'next/navigation'
+import { getUserByID } from '../middleware/helpers'
 
 export const getSession = async () => {
 	const supabase = await createClient()
@@ -12,4 +15,18 @@ export const getSession = async () => {
 	}
 
 	return data
+}
+
+export const getLoggedUser = async () => {
+	const session = await getSession()
+	if (!session?.user) {
+		redirect(HOME_DOMAIN)
+	}
+
+	const user = await getUserByID(session.user)
+	if (!user) {
+		redirect(HOME_DOMAIN)
+	}
+
+	return user
 }
